38fb63fd206c60779658a5c6bc5848a0afea7303
RedLeonFire <RedLeonFire@yandex.by>
2020-11-29 20:20:19 +0300

    Parsers: Folder has been creating. First parser The Guardian has been adding

---
+++ APIparsers/the_guardian_parser.py
@@ -0,0 +1,54 @@
+import requests
+
+from enum import Enum
+
+
+class ArticleTagsEnum(Enum):
+    all = 0,
+    sport = 1,
+    economy = 2,
+    science = 3,
+    musics = 4,
+    films = 5,
+    politics = 6
+
+
+class TheGuardianParser:
+    _MAX_PAGE_SIZE = 50
+    _API_SOURCE = 'http://content.guardianapis.com/search'
+    _API_KEY = '5dd8bff4-9d3a-43c5-8526-5743161d4992'
+
+    def __init__(self):
+        print('Create The Guardian Pasrser succ')
+
+    def get_articles(self, tag, count_articles):
+        page_count = count_articles // self._MAX_PAGE_SIZE + (count_articles % self._MAX_PAGE_SIZE > 0)
+        print(page_count)
+        articles = []
+
+        for page_number in range(1, page_count + 1):
+            response = self._get_response(tag, page_number)
+
+            if response['status'] == 'ok':
+                for item_article in response['results']:
+                    articles.append(item_article['fields']['bodyText'])
+                    count_articles -= 1
+                    if count_articles == 0:
+                        break
+            else:
+                break
+
+        return articles
+
+    def _get_response(self, tag, page):
+        return requests.get(
+            self._API_SOURCE,
+            params={
+                "order-by": "newest",
+                "show-fields": "bodyText",
+                "page-size": self._MAX_PAGE_SIZE,
+                "page": page,
+                "q": tag.name,
+                "api-key": self._API_KEY
+            }
+        ).json()['response']
--- sandbox/main.py
+++ sandbox/main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import json
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from APIparsers.the_guardian_parser import ArticleTagsEnum, TheGuardianParser
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    api_key = "5dd8bff4-9d3a-43c5-8526-5743161d4992"
+    #req = "https://content.guardianapis.com/search?q=\"mitochondrial%20donation\"&tag=politics/politics&from-date=2014-01-01&api-key=test"
+    #req = "https://content.guardianapis.com/tags&api-key=5dd8bff4-9d3a-43c5-8526-5743161d4992"
+    req = "http://content.guardianapis.com/search?order-by=newest&show-fields=bodyText&page-size=50&q=politics&api-key={}"
+
+    guardian_parser = TheGuardianParser()
+    articles = guardian_parser.get_articles(ArticleTagsEnum.all, 5)
+
+    #articles = response['response']['results']
+
+    #print(response)
+    print("Articles count =", len(articles))
+    #print(articles)
+    #for row in articles:
+        #print(row)
+     #   print(row['fields']['bodyText'])
+
+    print("Finish")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
