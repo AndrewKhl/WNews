39167265bf03897a74b055a2f31f5f5e14ad8afe
RedLeonFire <RedLeonFire@yandex.by>
2020-12-01 22:44:24 +0300

    Page: styles and filtred button has been adding

--- wnews/NewsSite/views.py
+++ wnews/NewsSite/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "news_list.html")
+    news_titles = ["{} title".format(i) for i in range(14)]
+    texts = ["Default text {}".format(i) for i in range(14)]
+
+    articles = [Article(title, text) for title, text in zip(news_titles, texts)]
+
+    return render(request, "news_list.html", locals())
+
+
+class Article:
+    title = ""
+    text = ""
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
