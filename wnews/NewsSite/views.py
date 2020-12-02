9396d461ff11b4351daae0e45b427872f0d1c6f4
RedLeonFire <RedLeonFire@yandex.by>
2020-12-02 21:10:47 +0300

    Site: button actions has been adding

--- wnews/NewsSite/views.py
+++ wnews/NewsSite/views.py
@@ -10,3 +10,12 @@ def home(request):
     articles, texts = text_manager.get_articles(ArticleTagsEnum.all, 15)
 
     return render(request, "news_list.html", locals())
+
+
+def filter_news(request):
+    text_manager = TextManager()
+    for item in ArticleTagsEnum:
+        if item.name in request.GET:
+            articles, texts = text_manager.get_articles(item, 15)
+    print("IIIIIIIII")
+    return render(request, "news_list.html", locals())
