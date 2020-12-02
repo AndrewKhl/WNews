2acb484251adea9f10c3e330586a4eb3b6381854
RedLeonFire <RedLeonFire@yandex.by>
2020-12-03 00:01:21 +0300

    Project: small improvements has been adding

--- wnews/NewsSite/views.py
+++ wnews/NewsSite/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render
 
-
 from .MachineLearning.TextManager import TextManager
 from .APIparsers.Models import ArticleTagsEnum
 
 
-def home(request):
-    text_manager = TextManager()
-    articles, texts = text_manager.get_articles(ArticleTagsEnum.all, 15)
+text_manager = TextManager()
 
+
+def home(request):
+    articles, texts = text_manager.get_articles(ArticleTagsEnum.all, 15)
     return render(request, "news_list.html", locals())
 
 
 def filter_news(request):
-    text_manager = TextManager()
     for item in ArticleTagsEnum:
         if item.name in request.GET:
             articles, texts = text_manager.get_articles(item, 15)
-    print("IIIIIIIII")
     return render(request, "news_list.html", locals())
